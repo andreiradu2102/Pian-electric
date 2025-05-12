cedd8676b1c982608deedecf47ad52fb510bd9cd
andreiradu2102 <119310849+andreiradu2102@users.noreply.github.com>
2025-05-12 10:30:40 +0300

    Changed main to now test buttons (made a piano keyboard with the buzzer)

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,37 @@
 #include <Arduino.h>
-#include <Wire.h>
 
-const int BUZZ = 9;
+const byte BUZZER_PIN = 9;
 
-const int notes[] = {262, 294, 330, 349, 392, 440, 494, 523};
+const byte keyPins[8] = {2, 3, 4, 5, 6, 7, A0, A1};
+
+const int  freqs[8]  = {262, 294, 330, 349, 392, 440, 494, 523};
+
+int currentKey = -1;
 
 void setup() {
   for (byte i = 0; i < 8; i++) {
-    tone(BUZZ, notes[i]);
-    delay(300);
-    noTone(BUZZ);
-    delay(50);
+    pinMode(keyPins[i], INPUT_PULLUP);
   }
 }
 
 void loop() {
+  int pressed = -1;
+
+  for (byte i = 0; i < 8; i++) {
+    if (digitalRead(keyPins[i]) == LOW) {
+      pressed = i;
+      break;
+    }
+  }
+
+  if (pressed != currentKey) {
+    if (pressed == -1) {
+      noTone(BUZZER_PIN);
+    } else {
+      tone(BUZZER_PIN, freqs[pressed]);
+    }
+    currentKey = pressed;
+  }
+
+  delay(5);
 }
